d44f873747b9fc358d17b6afc0a62ba02f6780e5
HiyallFitsum <70286522+HiyallFitsum@users.noreply.github.com>
2023-07-31 19:41:07 -0400

    Update bowl.js

--- bowl.js
+++ bowl.js
@@ -14,25 +14,27 @@ AFRAME .registerComponent("bowling", {
 
                 var ball = document.createElement("a-entity")
 
-                var camera = document.querySelector("#camera").object3D;
-
-                var scene = document.querySelector("#scene")
-
-                var direction = new THREE.Vector3()
-                camera.getWorldDirection(direction)
-
                 ball.setAttribute("gltf-model", "./models/bowling_ball/scene.gltf");
 
                 ball.setAttribute("scale", { x: 3, y: 3,  z: 3});
                 
-                ball.setAttribute("velocity", direction.multiplyScalar(-10))
+                var cam = document.querySelector("#camera")
+                var pos = cam.getAttribute("position");
+                ball.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z})
+
+                var camera = document.querySelector("#camera").object3D;
+
+                var direction = new THREE.Vector3()
+                camera.getWorldDirection(direction)
+
+                ball.setAttribute("velocity", direction.multiplyScalar(-20))
+
+                var scene = document.querySelector("#scene")
+
                 ball.setAttribute("dynamic-body", {
                     shape: "sphere",
                     mass: "0"
                 });
-                ball.setAttribute("material", {
-                    color: "black",
-                })
 
                 ball.addEventListener("collide", this.addCollision);
                 scene.appendChild(ball)
@@ -40,22 +42,23 @@ AFRAME .registerComponent("bowling", {
         })
     },
 
-    addCollision : function() {
+    addCollision : function(e) {
         var element = e.detail.target.el;
 
         var elementHit = e.detail.body.el;
 
-        if (elementHit.id.includes("#pin")){
-            elementHit.setAttribute("material", {
-                transparent: true,
-            })
+        if (elementHit.id.includes("pin")){
+            console.log(elementHit)
 
             var impulse = new CANNON.Vec3(-2, 2, 1);
-            var worldPoint = new CANNON.Vec3().copy(
-                elementHit.getAttribute("position")
-            );
+            var worldPoint = new CANNON.Vec3().copy(elementHit.getAttribute("position"));
+      
+            elementHit.body.applyForce(impulse, worldPoint);
 
-            elementHit.body.applyImpulse(impulse, worldPoint);
+            element.removeEventListener("collide", this.addCollision)
+
+            var scene = document.querySelector("#scene")
+            scene.removeChild(element);
         }
-    }
+    },
 })
