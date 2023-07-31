347edd9362d846bfc6032969da2736303f84ebf5
HiyallFitsum <70286522+HiyallFitsum@users.noreply.github.com>
2023-07-31 19:41:24 -0400

    Update gameObject.js

--- gameObject.js
+++ gameObject.js
@@ -1,4 +1,4 @@
-AFRAME.registerComponent("bowlingPins", {
+AFRAME.registerComponent("bowling-pins", {
     schema : {
 
     },
@@ -10,17 +10,31 @@ AFRAME.registerComponent("bowlingPins", {
     spawnPins : function() {
         let xPosition = -3;
 
-        for(var i = 0; i < 7; i++){
+        for(var i = 1; i < 11; i++){
             var pin = document.createElement("a-entity");
 
-            var positionX = xPosition + 1;
-            var position = { x: positionX, y: 0, z: -6}
+            var positionX = xPosition + 1*i;
+            if(i < 5){
+                var position = { x: positionX, y: 0, z: -6}
+            }else if(i < 8 && i>4){
+                var position = { x: positionX-3.6, y: 0, z: -5}
+            }else if(i < 10 && i>7){
+                var position = { x: positionX-6.25, y: 0, z: -4}
+            }else if(i>9){
+                var position = { x: positionX-7.95, y: 0, z: -3}
+            }
+            
             var scale = { x: 4, y: 4, z: 4}
 
             pin.setAttribute("id", "pin" + i);
-            pin.setAttribute("gltf-model", "./models/bowling_pin/scene.gltf")
+            pin.setAttribute("gltf-model", "./models/bowling_pin/scene.gltf");
+            //pin.setAttribute("geometry", {primitive: "sphere", radius: 0.25,})
             pin.setAttribute("position", position);
             pin.setAttribute("scale", scale);
+            pin.setAttribute("material", {color: "white"})
+            pin.setAttribute("dynamic-body", {mass: 1})
+
+            console.log(pin.getAttribute("id"));
 
             var scene = document.querySelector("#scene")
             scene.appendChild(pin)
